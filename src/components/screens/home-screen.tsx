33d4ae6d93d9183d0a702b3b0357b90d495539a8
Maxym <toper.one111@gmail.com>
2025-05-26 09:16:55 +0300

    feat: set up colors and typography

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -1,13 +1,15 @@
 const HomeScreen: React.FC = () => {
   return (
-    <div className="space-y-4">
-      <h1 className="text-3xl font-semibold text-blue-700">Головна сторінка</h1>
-      <p className="text-gray-700">
+    <div className="space-y-4 p-8">
+      <h1 className="h1 text-primary">Головна сторінка</h1>
+
+      <p className="body-1 text-gray-700">
         Вітаємо! Це тестовий лендінг з Tailwind CSS.
       </p>
+
       <a
         href="/login"
-        className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+        className="inline-block text-[16px] font-semibold leading-[150%] bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
       >
         Увійти
       </a>
