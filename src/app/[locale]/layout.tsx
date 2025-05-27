d0c284402eba39b50f7a38eaa7d7ab73261d71f2
Maxym <toper.one111@gmail.com>
2025-05-27 19:33:06 +0300

    feat: add internationlization

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,7 @@
+import { routing } from '@/i18n/routing';
 import type { Metadata } from 'next';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -8,16 +11,25 @@ export const metadata: Metadata = {
 
 type Props = Readonly<{
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }>;
 
-const RootLayout: React.FC<Props> = ({ children }) => {
+const RootLayout = async ({ children, params }: Props) => {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className="bg-gray-50 text-gray-900 min-h-screen">
         <header className="bg-blue-600 text-white px-6 py-4 text-xl font-bold shadow">
           Kola Innovations
         </header>
-        <main className="p-6">{children}</main>
+        <main className="p-6">
+          <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        </main>
       </body>
     </html>
   );
--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -1,19 +1,22 @@
 import GiftIcon from '@/assets/icons/gift.svg';
 import Link from 'next/link';
 import Button from '@/components/ui/button/button';
+import { useTranslations } from 'next-intl';
 
 const HomeScreen: React.FC = () => {
+  const t = useTranslations('landing');
+
   return (
     <div className="space-y-4 p-8">
-      <h1 className="h1 text-primary">Головна сторінка</h1>
+      <h1 className="h1 text-primary">{t('title')}</h1>
 
       <p className="body-1 text-gray-700">
-        Вітаємо! Це тестовий лендінг з Tailwind CSS. <GiftIcon />
+        {t('description')} <GiftIcon />
       </p>
 
       <Link href="/login">
         <Button variant="primary" size="md" color="red">
-          Увійти
+          {t('login')}
         </Button>
       </Link>
     </div>
