c72c59913300603f20c4117fcb2fe323bb2791ba
Maxym <toper.one111@gmail.com>
2025-06-23 17:16:39 +0300

    chore: router back

--- src/components/modules/profile/header.tsx
+++ src/components/modules/profile/header.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import ArrowLeft from '@/assets/icons/arrow-left.svg';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   title: string;
@@ -6,9 +9,11 @@ type Props = {
 };
 
 const ProfileHeader: React.FC<Props> = ({ title, actionElement }) => {
+  const router = useRouter();
+
   return (
     <div className="flex justify-between items-center">
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-4 items-center" onClick={() => router.back()}>
         <ArrowLeft />
         <p className="h6 text-dark-900">{title}</p>
       </div>
