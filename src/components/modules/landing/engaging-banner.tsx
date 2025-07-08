5bf636f83b4d0889511131e45a115b36a035e0d9
Maxym <toper.one111@gmail.com>
2025-07-08 15:42:40 +0300

    feat: motivation section adaptation

--- src/components/modules/landing/engaging-banner.tsx
+++ src/components/modules/landing/engaging-banner.tsx
@@ -18,15 +18,20 @@ const EngagingBanner: React.FC<Props> = ({
   swiggleClassName,
 }) => {
   return (
-    <div className="bg-primary-100 p-12 rounded-[40px] flex justify-between items-end gap-8 relative">
-      <div className="max-w-[684px]">
-        <h3 className="h3 text-dark-900 mb-4">{heading}</h3>
-        <p className="h6-rg text-dark-700">{subheading}</p>
+    <div className="bg-primary-100 lg:p-12 py-[30px] px-[20px] rounded-[12px] lg:rounded-[40px] flex items-center sm:items-start flex-col gap-8 relative">
+      <div className="flex gap-8 items-end justify-between w-full">
+        <div className="max-w-[684px]">
+          <h3 className="h3 text-dark-900 mb-4">{heading}</h3>
+          <p className="h6-rg text-dark-700">{subheading}</p>
+        </div>
+        <div className="hidden lg:block">{actionElement}</div>
       </div>
-      {actionElement}
 
       <div
-        className={cn('absolute top-[100px] left-[750px]', swiggleClassName)}
+        className={cn(
+          'relative left-[120px] sm:static lg:absolute scale-x-[-0.7] scale-70 sm:scale-100 sm:scale-x-100 lg:top-[100px] lg:left-[750px]',
+          swiggleClassName,
+        )}
       >
         <SwiggleArrow />
       </div>
@@ -36,7 +41,7 @@ const EngagingBanner: React.FC<Props> = ({
         alt="tik tok label"
         width={270}
         height={85}
-        className="absolute -top-[30px] right-[200px]"
+        className="relative sm:static lg:absolute scale-[130%] sm:scale-100 right-[20px] bottom-[50px] lg:-top-[30px] lg:right-[200px]"
       />
 
       <Image
@@ -44,8 +49,10 @@ const EngagingBanner: React.FC<Props> = ({
         alt="spotify label"
         width={270}
         height={85}
-        className="absolute top-[30px] -right-[105px]"
+        className="relative bottom-[50px] lg:bottom-auto lg:left-auto left-[50px] scale-[120%] sm:scale-100 sm:static lg:absolute lg:top-[30px] lg:-right-[105px]"
       />
+
+      <div className="lg:hidden self-stretch">{actionElement}</div>
     </div>
   );
 };
--- src/components/modules/landing/sections/motivation-section.tsx
+++ src/components/modules/landing/sections/motivation-section.tsx
@@ -6,11 +6,20 @@ import Button from '@/components/ui/button/button';
 const MotivationSection: React.FC = () => {
   const t = useTranslations('landing.motivation');
   return (
-    <MaxWidthWrapper className="pb-40">
+    <MaxWidthWrapper className="pb-8 sm:pb-40">
       <EngagingBanner
         heading={t('heading')}
         subheading={t('subHeading')}
-        actionElement={<Button size="lg">{t('getStarted')}</Button>}
+        actionElement={
+          <>
+            <Button size="lg" className="hidden sm:flex w-full lg:w-auto">
+              {t('getStarted')}
+            </Button>
+            <Button size="md" className="sm:hidden w-full lg:w-auto">
+              {t('getStarted')}
+            </Button>
+          </>
+        }
       />
     </MaxWidthWrapper>
   );
