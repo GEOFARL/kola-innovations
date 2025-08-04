8ded21592b8b33e2608951eaa1685c2a7bc1c7c5
Maxym <toper.one111@gmail.com>
2025-08-04 22:34:29 +0300

    Sync from monorepo commit 9bd53a64779e4a8389bd853bddd3d98e4e55c55b: fix: user error (2025-08-04 22:34:29)

--- src/components/modules/shared/layouts/common-layout.tsx
+++ src/components/modules/shared/layouts/common-layout.tsx
@@ -20,7 +20,7 @@ const CommonLayout: React.FC<Props> = ({
 }) => {
   return (
     <>
-      <Header user={User.fromClerk(user)} />
+      <Header user={user ? User.fromClerk(user) : null} />
       <MaxWidthWrapper className="grid lg:grid-cols-[220px_1fr_342px] h-[calc(100vh-53px)] lg:h-[calc(100vh-88px)] px-0 sm:px-0 lg:px-4 xl:px-8">
         <aside className="sticky hidden lg:block top-0 h-full overflow-y-auto border-r border-dark-100 bg-white">
           {leftAsideContent}
